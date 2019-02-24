6c9b1d7e7416f9da5d64c8615cb97cf0e36941b0
showduhtung <shaun.tung@gmail.com>
2019-02-24 17:57:03 -0500

    kinda fixed the responsiveness.. but it's kinda hacky

--- client/index.js
+++ client/index.js
@@ -9,7 +9,7 @@ import { Controller, Scene } from 'react-scrollmagic';
 import { StickyContainer, Sticky } from 'react-sticky';
 import AOS from 'aos';
 
-AOS.init({ offset: 500, duration: 600, easing: 'ease-in-sine', delay: 200 });
+// AOS.init({ offset: 500, duration: 600, easing: 'ease-in-sine', delay: 200 });
 ReactDOM.render(
   <div>
     <div className="home-page" id="homePage">
