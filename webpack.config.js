33a05b9c7455879fa6b307e8ec51d872dce05ebd
showduhtung <shaun.tung@gmail.com>
2019-03-11 09:22:27 -0400

    re-established dependencies but also added back nodemon

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ module.exports = {
   entry: ['@babel/polyfill', './client/index.js'], // entry is for webpack to front page
   mode: 'development',
   output: {
-    path: __dirname, // assumes your bundle.js will also be in the root of your project folder
     filename: './public/bundle.js',
+    path: __dirname, // assumes your bundle.js will also be in the root of your project folder
   },
   devtool: 'source-maps',
   module: {
