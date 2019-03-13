0119e26a47c7e6e4f1c5e7f8fead2c85cba216df
showduhtung <shaun.tung@gmail.com>
2019-03-12 21:20:45 -0400

    the home page has the feel i want, just need to rearrange everything. ask for help here

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,33 +1,49 @@
 import React from 'react';
+import { Transform } from 'stream';
 
 export default class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <div className="front-page-name">
-          <div className="front-page-name-name">
+        <div className="front-page-headline">
+          <div>
             <h1 style={{ fontWeight: 100 }}>
               <u>S</u>HAUN <u>TUNG</u>
-              {/* <div>Welcome to My Portfolio</div> */}
             </h1>
           </div>
-          <div className="front-page-name-welcome">
+          <div>
             <h1 style={{ fontWeight: 100 }}>Welcome to My Portfolio</h1>
           </div>
         </div>
 
         <div className="front-page-text">
-          <h1 className="quote-part-1">"FLOAT LIKE A BUTTERFLY</h1>
-          <h3 className="quote-part-2">STING LIKE A BEE" - M. ALI</h3>
+          <h1
+            className="quote-part-1"
+            style={{ fontWeight: 100, backgroundColor: 'yellow' }}
+          >
+            "FLOAT LIKE A BUTTERFLY
+          </h1>
+          <h3
+            className="quote-part-2"
+            style={{ fontWeight: 100, backgroundColor: 'yellow' }}
+          >
+            STING LIKE A BEE" - M. ALI
+          </h3>
         </div>
         <div className="front-page-blurb">
-          <h2>My dad told me this quote when I was young.</h2>
-          <h2>To float like a butterfly, and to be like a</h2>
-          <h2>bee was to be adapable and effective.</h2>
+          <h2 style={{ backgroundColor: 'orange' }}>
+            My dad told me this quote when I was young.
+          </h2>
+          <h2 style={{ backgroundColor: 'orange' }}>
+            To float like a butterfly, and to be like a
+          </h2>
+          <h2 style={{ backgroundColor: 'orange' }}>
+            bee was to be adapable and effective.
+          </h2>
         </div>
         <div className="front-page-moreblurb">
-          <h2>You got problems?</h2>
-          <h2>I got solutions</h2>
+          <h2 style={{ backgroundColor: 'red' }}>You got problems?</h2>
+          <h2 style={{ backgroundColor: 'red' }}>I got solutions</h2>
         </div>
       </div>
     );
